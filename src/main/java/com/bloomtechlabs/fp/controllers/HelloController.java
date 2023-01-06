3cdb6b3b15b50539bdacb0395c5371b05833b64c
Tom Tarpey <tomtarpeydev@gmail.com>
2023-01-06 00:17:44 +0000

    added pg folder and some mock table inside the pg folder to persist the folder in git. also refactored the mock table in to a mock entity. and changed the message inside the hello world controller to output the general message for labs

--- src/main/java/com/bloomtechlabs/fp/controllers/HelloController.java
+++ src/main/java/com/bloomtechlabs/fp/controllers/HelloController.java
@@ -9,7 +9,7 @@ import java.util.Base64;
 public class HelloController {
     @GetMapping
     public String index() {
-        byte[] decodedBytes = Base64.getDecoder().decode("SGVsbG8gV29ybGQ=");
+        byte[] decodedBytes = Base64.getDecoder().decode("VGltZTJDb2Rl");
         String decodedString = new String(decodedBytes);
         return decodedString;
     }
