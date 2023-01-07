3fd40c5d44bf2ee222e1a722d6d293c2742adfcc
ax56 <amsanon1@protonmail.com>
2023-01-07 08:42:54 -0500

    resources_html_template

--- src/main/java/com/bloomtechlabs/fp/security/SecurityConfig.java
+++ src/main/java/com/bloomtechlabs/fp/security/SecurityConfig.java
@@ -13,9 +13,28 @@ import org.springframework.security.web.SecurityFilterChain;
 @EnableWebSecurity
 public class SecurityConfig {
     
+    /**
+     * This method will provide an instance of {@link SecurityFilterChain}
+     * @param http {@link HttpSecurity} object
+     * @return {@link SecurityFilterChain} object
+     * @throws Exception if any error occurs
+     */
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         return http.oauth2Login()
                  .and().build();
     }
 }
+
+/*
+ You can further configure the HttpSecurity instance to require authentication
+ on all or certain paths. For example,
+ to require authentication on all paths except the home page:
+
+http.authorizeRequests()
+      .mvcMatchers("/").permitAll()
+      .anyRequest().authenticated()
+      .and().oauth2Login();
+*/
+
+
