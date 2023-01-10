9b29cd1248b189544fcbcedbd0a542e4f73edfeb
SamVVBB <hedinsamuel1995@gmail.com>
2023-01-10 12:51:24 -1000

    created EducationHistory entity

---
+++ src/main/java/com/bloomtechlabs/fp/entities/EducationHistory.java
@@ -0,0 +1,88 @@
+package com.bloomtechlabs.fp.entities;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@Table(name ="education_histories")
+public class EducationHistory {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "client_id")
+    private Long clientId;
+
+    @Column(name = "school_name")
+    private String schoolName;
+
+    @Column(name = "level")
+    private String level;
+
+    @Column(name = "start_date")
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date startDate;
+
+    @Column(name = "end_date")
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date endDate;
+
+    public EducationHistory() {
+    }
+
+    public EducationHistory(Long clientId, String schoolName, String level, Date startDate, Date endDate) {
+        this.clientId   = clientId;
+        this.schoolName = schoolName;
+        this.level      = level;
+        this.startDate  = startDate;
+        this.endDate    = endDate;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getClientId() {
+        return clientId;
+    }
+
+    public void setClientId(Long clientId) {
+        this.clientId = clientId;
+    }
+
+    public String getSchoolName() {
+        return schoolName;
+    }
+
+    public void setSchoolName(String schoolName) {
+        this.schoolName = schoolName;
+    }
+
+    public String getLevel() {
+        return level;
+    }
+
+    public void setLevel(String level) {
+        this.level = level;
+    }
+
+    public Date getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(Date startDate) {
+        this.startDate = startDate;
+    }
+
+    public Date getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(Date endDate) {
+        this.endDate = endDate;
+    }
+}
