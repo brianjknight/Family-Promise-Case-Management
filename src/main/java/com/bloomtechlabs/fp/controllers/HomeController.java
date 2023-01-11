5cf43260b035d77d444bad909e54060946cbcdc0
ax56 <amsanon1@protonmail.com>
2023-01-11 08:24:31 -0500

    removed_custom_implementations

--- src/main/java/com/bloomtechlabs/fp/controllers/HomeController.java
+++ src/main/java/com/bloomtechlabs/fp/controllers/HomeController.java
@@ -2,24 +2,13 @@ package com.bloomtechlabs.fp.controllers;
 
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.core.oidc.user.OidcUser;
-import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-/**
- * {@link HomePageController} is a controller for the home page.
- */
-@Controller
-public class HomePageController {
+@RestController
+public class HomeController {
     
-    /**
-     * {@link HomePageController#home(Model, OidcUser)} show profile
-     * information associated with the logged-in users
-     *
-     * @param model the model
-     * @param principal the authentication principal
-     * @return the home page index
-     */
     @GetMapping("/")
     public String home(Model model, @AuthenticationPrincipal OidcUser principal) {
         if (principal != null) {
